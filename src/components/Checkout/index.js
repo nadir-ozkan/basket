5799e45b26aa3e59a5e7985182528649a4579189
nadmez <nadmez@yahoo.com>
2022-08-28 15:10:29 +0300

    Initial commit

---
+++ src/components/Checkout/index.js
@@ -0,0 +1,9 @@
+const Checkout = () => {
+  return (
+    <div className="product-list">
+      <h1>Checkout page here!</h1>
+    </div>
+  );
+};
+
+export default Checkout;
---
+++ src/components/ProductList/index.js
@@ -0,0 +1,9 @@
+const ProductList = () => {
+  return (
+    <div className="product-list">
+      <h1>Product List here!</h1>
+    </div>
+  );
+};
+
+export default ProductList;
